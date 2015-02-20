abb18b5660972c64953c9191f0237903d6298e7a
Jamzzebob <jamesrevell@rocketmail.com>
2015-02-20 21:30:17 +0000

    Create Payload.py

---
+++ Payload.py
@@ -0,0 +1,23 @@
+import sys
+import math
+import time
+import clr
+clr.AddReference("MissionPlanner")
+import MissionPlanner
+clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
+from MissionPlanner.Utilities import Locationwp
+
+H_lat =                 #Home Latitude
+H_long =                #Home Longitude
+
+DZ_lat =                #Drop Zone Latitude
+DZ_Long =               #Drop Zone Longitude
+
+WPA_lat =               #Waypoint A Latitude
+WPA_long =              #Waypoint A Longitude
+
+WPB_lat =               #Waypoint B Latitude
+WPB_long =              #Waypoint B Longitude
+
+WPC_lat =               #Waypoint C Latitude
+WPC_long =              #Waypoint C Longitude
