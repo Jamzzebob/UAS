4a7d54f5890ddbd0ee690cc53c5c8dee150c5987
Jamzzebob <jamesrevell@rocketmail.com>
2015-02-25 11:33:27 +0000

    Create ImageRecognition.py

---
+++ ImageRecognition.py
@@ -0,0 +1,29 @@
+import sys
+import math
+import time
+import clr
+clr.AddReference("MissionPlanner")
+import MissionPlanner
+clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
+from MissionPlanner.Utilities import Locationwp
+
+def imrec(DZ_lat,DZ_long)
+
+R = 6371000                                             #Radius of the Earth
+
+DZ_lat = math.radians(DZ_lat)
+DZ_long = math.radians(DZ_long)
+
+lat1 = math.radians(cs.lat)
+long1 = math.radians(cs.long)
+
+a = math.sin((DZ_latr-lat1)/2) * math.sin((DZ_latr-lat1)/2) + math.cos(lat1) * math.cos(DZ_latr) * math.sin((DZ_longr-long1)/2) * math.sin((DZ_longr-long1)/2)t
+c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+d = R * c
+
+y = math.sin(DZ_longr-long1) * math.cos(DZ_latr)
+x = math.cos(lat1) * math.sin(DZ_latr) - math.sin(lat1)*math.cos(DZ_latr)*math.cos(DZ_longr-long1)
+brng = math.atan2(y, x)
+theta = math.degrees(brng)
+
+return x,y
