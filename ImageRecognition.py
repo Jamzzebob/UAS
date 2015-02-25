c51d26ab8b4a8f24d43334dc54042eea3ff18fdd
Jamzzebob <jamesrevell@rocketmail.com>
2015-02-25 12:00:53 +0000

    Update ImageRecognition.py

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -21,9 +21,7 @@ a = math.sin((DZ_latr-lat1)/2) * math.sin((DZ_latr-lat1)/2) + math.cos(lat1) * m
 c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
 d = R * c
 
-y = math.sin(DZ_longr-long1) * math.cos(DZ_latr)
-x = math.cos(lat1) * math.sin(DZ_latr) - math.sin(lat1)*math.cos(DZ_latr)*math.cos(DZ_longr-long1)
-brng = math.atan2(y, x)
-theta = math.degrees(brng)
+x = d * math.sin(DZ_longr-long1) * math.cos(DZ_latr)
+y = d * math.cos(lat1) * math.sin(DZ_latr) - math.sin(lat1)*math.cos(DZ_latr)*math.cos(DZ_longr-long1)
 
 return x,y
