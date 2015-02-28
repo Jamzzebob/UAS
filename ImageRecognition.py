b79e308bb6666a40a7a0447b08e104dc386f6f14
Jamzzebob <jamesrevell@rocketmail.com>
2015-02-28 13:57:32 +0000

    Update ImageRecognition.py

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -7,21 +7,28 @@ import MissionPlanner
 clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
 from MissionPlanner.Utilities import Locationwp
 
-def imrec(DZ_lat,DZ_long)
+def imrec(DZ_lat,DZ_long,cslat,cslng):
+    
+    R = 6371000                                             #Radius of the Earth
+    
+    DZ_latr = math.radians(DZ_lat)
+    DZ_longr = math.radians(DZ_long)
+    
+    lat1 = math.radians(cslat)
+    long1 = math.radians(cslng)
+    
+    a = math.sin((DZ_latr-lat1)/2) * math.sin((DZ_latr-lat1)/2) + math.cos(lat1) * math.cos(DZ_latr) * math.sin((DZ_longr-long1)/2) * math.sin((DZ_longr-long1)/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R * c
 
-R = 6371000                                             #Radius of the Earth
-
-DZ_lat = math.radians(DZ_lat)
-DZ_long = math.radians(DZ_long)
-
-lat1 = math.radians(cs.lat)
-long1 = math.radians(cs.long)
-
-a = math.sin((DZ_latr-lat1)/2) * math.sin((DZ_latr-lat1)/2) + math.cos(lat1) * math.cos(DZ_latr) * math.sin((DZ_longr-long1)/2) * math.sin((DZ_longr-long1)/2)t
-c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-d = R * c
-
-x = d * math.sin(DZ_longr-long1) * math.cos(DZ_latr)
-y = d * math.cos(lat1) * math.sin(DZ_latr) - math.sin(lat1)*math.cos(DZ_latr)*math.cos(DZ_longr-long1)
-
-return x,y
+    print(d)
+    
+    x = (math.sin(DZ_longr-long1) * math.cos(DZ_latr))
+    y = (math.cos(lat1) * math.sin(DZ_latr) - math.sin(lat1)*math.cos(DZ_latr)*math.cos(DZ_longr-long1))
+    brng = math.atan2(x, y)
+    brng = math.degrees(brng)
+    brng = (brng+360)%360
+    print(brng)
+    x = d * math.sin(math.radians(brng))
+    y = d * math.cos(math.radians(brng))
+    return x,y
