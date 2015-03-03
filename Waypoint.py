1f911b2165c353de5652e30137c34172fcd4267e
Jamzzebob <jamesrevell@rocketmail.com>
2015-03-03 20:11:23 +0000

    Update Waypoint.py

--- Waypoint.py
+++ Waypoint.py
@@ -2,6 +2,6 @@ import sys
 import math
 import time
 
-DZ_lat = -35.361259                 #Drop Zone Latitude
-DZ_long = 149.162018               #Drop Zone Longitude
-r = 50								#Loiter Radius
+DZ_lat = 53.926174                #Drop Zone Latitude
+DZ_long = -0.978281               #Drop Zone Longitude
+r = 70								#Loiter Radius
